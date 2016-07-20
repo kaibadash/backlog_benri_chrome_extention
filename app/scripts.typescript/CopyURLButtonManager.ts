3de691de4eb0f3b777cea54e6be32aeb2b32479c
kaibazawa.dai <kaibadash@gmail.com>
2016-07-20 18:36:56 +0900

    Remove url hash when copy url

--- app/scripts.typescript/CopyURLButtonManager.ts
+++ app/scripts.typescript/CopyURLButtonManager.ts
@@ -6,20 +6,21 @@
 class CopyURLButtonManager {
     private imgURL: string = chrome.extension.getURL("images/link.png");
     private idText: string = "backlog-benri-text-for-clipboard";
-       
+
     run() {
         this.createDom();
     }
-    
+
     needToExec() : Boolean {
         return location.href.lastIndexOf(".backlog.jp/view/") > 0;
     }
-    
+
     private createDom() : JQuery {
-        var parent: JQuery = $("#copyKey-help").parent();  
+        var parent: JQuery = $("#copyKey-help").parent();
         var button: JQuery = $('<input type="image" src="' + this.imgURL + '" width="20px" height="20px" />');
         button.click(() => {
-            chrome.runtime.sendMessage({text: location.href});
+            var url: String = location.href.split("#")[0];
+            chrome.runtime.sendMessage({text: url});
         });
         parent.append(button);
         return parent;
